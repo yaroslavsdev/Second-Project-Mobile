064c3c2b83a69be46a8a7815e35303c3899addb8
yaroslavsdev <yaroslav.safronov.09@yandex.ru>
2026-05-29 13:01:20 +0300

    Сервер готов

--- Server/main.py
+++ Server/main.py
@@ -139,5 +139,7 @@ def add_todo():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    # app.run(host='localhost', port=8080, debug=True)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
+
+    # Для локальной сети
+    # app.run(host='0.0.0.0', port=8080, debug=True)
